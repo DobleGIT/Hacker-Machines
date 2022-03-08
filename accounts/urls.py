9f1259be0700b240b336c9b95b544af0fd5b6914
DobleGIT <jaime.36111@gmail.com>
2022-03-08 19:41:43 +0100

    Make login.

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,7 +5,9 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('maquinas/<str:pk_maquina>', views.maquinas, name="maquinas"),
-    path('home/', views.home),
-    path('iniciarSesion/', views.iniciarSesion),
+    path('home/', views.home, name="home"),
+    path('logout/', views.logoutUser, name='logout'),
+    path('login/', views.loginUsername, name='login'),
     path('registrarse/', views.registrarse, name='register'),
+    path('dashboard/', views.dashboard, name='dashboard'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm  
+from django.contrib.auth import authenticate,login,logout
+
+from django.contrib.auth.decorators import login_required # lo usamos para los decorators, para que no se pueda acceder a diferentes paginas sin estar loggeado
 
 # Create your views here.
 from .models import *
 from .forms import CreateUserForm
 
 def home(request):
+	messages.warning(request, 'Your account expires in three days.')
 	return render(request, 'accounts/home.html')
 
+@login_required(login_url='login') #comprobamos que esté loggeado
+def dashboard(request):
+	return render(request, 'accounts/dashboard.html')
+
+@login_required(login_url='login')
 def maquinas(request, pk_maquina):
 	maquinas = Maquina.objects.all()
 	maquina_individual = Maquina.objects.get(nombre_maquina=pk_maquina) 	#esto quiere decir que a la url se le pasa la clave primaria de la maquina
@@ -17,19 +26,42 @@ def maquinas(request, pk_maquina):
 	contexto = {'lista_maquinas':maquinas} 						#este es el diccionario que le pasamos a la url 
 	return render(request, 'accounts/maquinas.html',contexto)
 
-def iniciarSesion(request):
-	return render(request, 'accounts/iniciarSesion.html')
+def loginUsername(request):
+
+	contexto={}
+	if request.method == 'POST':
+		username = request.POST.get('username')
+		password = request.POST.get('password')
+
+		user = authenticate(request, username=username, password=password)
+
+		if user is not None:		#si se autentifica bien 
+			login(request, user)
+			return redirect('dashboard')
+		else:
+			messages.info(request, 'El usuario o la password es incorrecta') #ESTO NO FUNCIONA ARREGLARLO
+		
+	
+	return render(request, 'accounts/login.html',contexto)
+
+def logoutUser(request):
+	logout(request)
+	return redirect('login')
 
 def registrarse(request):
-	form = CreateUserForm()
-	
-	if request.method == 'POST':
-		form = CreateUserForm(request.POST)
-		if form.is_valid():
-			form.save()
-			messages.success(request, 'Account created successfully')		
-		
 
-	contexto = {'form':form}
-	return render(request, 'accounts/registrarse.html', contexto)
-# Create your views here.
+	if request.user.is_authenticated: #esto se usa para comprobar que si está autentificado no pueda acceder a registrarse
+		return redirect('home')
+	else:
+		form = CreateUserForm()
+		
+		if request.method == 'POST':
+			form = CreateUserForm(request.POST)
+			if form.is_valid():
+				form.save()
+				redirect('login')
+					
+			
+
+		contexto = {'form':form}
+		return render(request, 'accounts/registrarse.html', contexto)
